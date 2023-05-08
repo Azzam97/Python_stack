df3228600d5564ed0a50e73629abe9d69379e014
Azzam Ahmad <Azzam.ahmad97@gmail.com>
2023-05-08 22:15:37 +0300

    OOP example commit

---
+++ _python/OOP/examples.py
@@ -0,0 +1,29 @@
+class user:
+    def __init__(self,name,email_address):
+        self.name = name
+        self.email = email_address
+        self.account = BankAccount(int_rate=0.02,balance=0)
+
+
+class BankAccount:
+    def __init__(self, int_rate=0.02, balance=0): # don't forget to add some default values for these parameters!
+        self.int_rate = int_rate
+        self.balance = balance
+        # don't worry about user info here; we'll involve the User class soon
+    def deposit(self, amount):
+        self.balance += amount
+        return self
+    def withdraw(self, amount):
+        self.balance -= amount
+        return self
+    def display_account_info(self):
+        print ("Balance: ", self.interest_yield)
+        return self
+    def yield_interest(self):
+        self.interest_yield = (self.int_rate * self.balance) + self.balance
+        return self
+
+Azzam = user("Azzam Khames","Azzam.ahmad97@gmail.com")
+Azzam.account.deposit(100)
+print(Azzam.account.balance)
+print(Azzam.account)
