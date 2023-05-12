7eccea809962010d19d7d77d7f7abdd9d91a346d
Azzam Ahmad <Azzam.ahmad97@gmail.com>
2023-05-12 20:30:49 +0300

    code refinment based on the code review

--- _python/OOP/users_with_bank_accounts.py
+++ _python/OOP/users_with_bank_accounts.py
@@ -1,27 +1,47 @@
-class user:
+class User:
     def __init__(self,name,email_address):
         self.name = name
         self.email = email_address
-        self.account = BankAccount(int_rate=0.02,balance=0)
+        self.account = BankAccount()
+
+    def make_deposit(self,amount):
+        self.account.balance += amount
+        return self
+    
+    def make_withdrawal(self,amount):
+        self.account.balance -= amount
+        return self
+    
+    def display_user_balance(self):
+        print(f"{self.name}'s balance is: {self.account.balance}")
+        return self
 
 
 class BankAccount:
     def __init__(self, int_rate=0.02, balance=0):
         self.int_rate = int_rate
         self.balance = balance
+
     def deposit(self, amount):
         self.balance += amount
         return self
+    
     def withdraw(self, amount):
         self.balance -= amount
         return self
+    
     def display_account_info(self):
         print ("Balance: ", self.interest_yield)
         return self
+    
     def yield_interest(self):
         self.interest_yield = (self.int_rate * self.balance) + self.balance
         return self
 
-Azzam = user("Azzam Khames","Azzam.ahmad97@gmail.com")
-Azzam.account.deposit(100)
-print(Azzam.account.balance)
+saving_account = BankAccount(0.02,1000)
+current_account = BankAccount(0,600)
+azzam = User("Azzam Khames","Azzam.ahmad97@gmail.com")
+azzam.account = saving_account
+azzam.make_deposit(100).display_user_balance()
+azzam.account = current_account
+azzam.make_withdrawal(300).display_user_balance()
