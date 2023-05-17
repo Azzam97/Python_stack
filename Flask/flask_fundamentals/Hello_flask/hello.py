76bd74cd6e12bb9e9ef785aa119d914c13b2868f
Azzam Ahmad <Azzam.ahmad97@gmail.com>
2023-05-17 20:11:12 +0300

    understanding routing assignment commit

--- Flask/flask_fundamentals/Hello_flask/hello.py
+++ Flask/flask_fundamentals/Hello_flask/hello.py
@@ -1,15 +1,22 @@
-from flask import Flask, render_template  # Import Flask to allow us to create our app
-app = Flask(__name__)    # Create a new instance of the Flask class called "app"
-@app.route('/')          # The "@" decorator associates this route with the function immediately following
-def hello_world():
-    return render_template('index.html', phrase="hello", times=5)  # Return the string 'Hello World!' as a response
-# @app.route('/success')
-# def success():
-#     return "success"
-# @app.route('/hello/<name>')
-# def hello(name):
-#     print(name)
-#     return "Hello," + name
-if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
-    app.run(debug=True)    # Run the app in debug mode.
+from flask import Flask
+app = Flask(__name__)
 
+@app.route('/')
+def hello_world():
+    return "Hello World!"
+
+@app.route('/dojo')
+def dojo():
+    return " Dojo"
+
+@app.route('/<name>')
+def faliure(name):
+    return "Hi "+ name
+
+@app.route('/repeate/<num>/<name>')
+def repeate(name, num):
+    repetition = int (num)       #convert the number from string into an int to be used in the return statement
+    return name * repetition
+
+if __name__=="__main__":
+    app.run(debug=True)
