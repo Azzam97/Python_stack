60f47a004909604cf7de56331329722ef5c54b98
Azzam Ahmad <Azzam.ahmad97@gmail.com>
2023-05-18 20:52:39 +0300

    making the checkerboard assignment more dynamic

--- Flask/flask_fundamentals/Checkerboard/checkerboard.py
+++ Flask/flask_fundamentals/Checkerboard/checkerboard.py
@@ -11,11 +11,20 @@ def checkerboard1():
 
 @app.route('/<int:ver_num>')
 def checkerboard2(ver_num):
-    return render_template('index_vertical.html', ver_num)
+    y = ver_num
+    return render_template('index_row.html', row = y)
 
 @app.route('/<int:hor_num>/<int:ver_num>')
 def checkerboard3(hor_num,ver_num):
-    return render_template('index_horizental_vertical.html',hor_num , ver_num)
+    x = hor_num
+    y = ver_num
+    return render_template('index_row_line.html', row = x , line = y)
+
+@app.route('/<int:hor_num>/<int:ver_num>/<color1>/<color2>')
+def checkerboard4(hor_num, ver_num, color1, color2):
+    x = hor_num
+    y = ver_num
+    return render_template('index_new_colors.html', row = x, line = y, colour1 = color1, colour2 = color2)
 
 if __name__=='__main__':
     app.run(debug=True)
