3ff6b6d78e332ab76fd03d0d43612cc09ffe449b
Azzam Ahmad <Azzam.ahmad97@gmail.com>
2023-05-18 20:14:09 +0300

    making the variable an integer in the url

--- Flask/flask_fundamentals/Playground/playground.py
+++ Flask/flask_fundamentals/Playground/playground.py
@@ -9,13 +9,13 @@ app = Flask(__name__)
 def level_1():
     return render_template('index.html')
 
-@app.route('/play/<num>')
+@app.route('/play/<int:num>')
 def level_2(num):
-    return render_template('index_number.html', number=int(num))
+    return render_template('index_number.html', num)
 
-@app.route('/play/<num>/<color>')
+@app.route('/play/<int:num>/<color>')
 def level_3(num , color):
-    return render_template('index_number_color.html', number=int(num), colour=color)
+    return render_template('index_number_color.html', num, colour=color)
 
 if __name__=="__main__":
     app.run(debug=True)
