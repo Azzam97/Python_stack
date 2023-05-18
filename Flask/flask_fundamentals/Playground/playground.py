268613d868bc0d9835b29c28d6be4838cd80ca79
Azzam Ahmad <Azzam.ahmad97@gmail.com>
2023-05-18 20:22:52 +0300

    accepting an integer in the url

--- Flask/flask_fundamentals/Playground/playground.py
+++ Flask/flask_fundamentals/Playground/playground.py
@@ -1,5 +1,5 @@
 """
-    this program runs an app to give me a diffirent number of boxes based on the route used
+    this program runs an app to give me a diffirent number of boxes based on the route
 """
 
 from flask import Flask, render_template
@@ -11,11 +11,13 @@ def level_1():
 
 @app.route('/play/<int:num>')
 def level_2(num):
-    return render_template('index_number.html', num)
+    x = num
+    return render_template('index_number.html', number = x)
 
 @app.route('/play/<int:num>/<color>')
 def level_3(num , color):
-    return render_template('index_number_color.html', num, colour=color)
+    x = num
+    return render_template('index_number_color.html', number = x, colour = color)
 
 if __name__=="__main__":
     app.run(debug=True)
