343d2715fc667d3e92b2d178328f64aff73fd46a
Azzam Ahmad <Azzam.ahmad97@gmail.com>
2023-05-26 05:18:24 +0300

    refining the ninja gold assignment

--- Flask/flask_fundamentals/Ninja_gold/server.py
+++ Flask/flask_fundamentals/Ninja_gold/server.py
@@ -7,18 +7,23 @@ app = Flask(__name__)
 app.secret_key = "ninja"
 
 text = []
-gold = 0
-amount = 0
-color = ''
 
 
 @app.route('/')
 def game_start():
-    session['gold'] = session['gold']
-    session['text'] = text
-    session['amount'] = amount + session['amount']
-    return render_template('index.html', gold=session['gold'], text=session['text'], amount=session['amount'])
+    if 'gold'  not in session:
+        session['gold'] = 0
+        session['amount'] = 0
+        session['text'] = []
+    gold = session['gold']
+    text = session['text']
+    amount = session['amount'] 
+    return render_template('index.html', gold = gold, text = text, amount = amount)
 
+@app.route('/clear', methods = ['POST'])
+def clear():
+    session.clear()
+    return redirect('/')
 
 @app.route('/process_money', methods=['POST'])
 def process():
@@ -29,29 +34,30 @@ def process():
         amount = random.randint(10, 20)
         session['gold'] += amount
         session['amount'] = amount
-        text.insert(0, f"earned {amount} golds from the farm ! {timestamp}")
+        text = (0, f"earned {amount} golds from the farm ! {timestamp}")
     elif method == 'cave':
         amount = random.randint(5, 10)
         session['gold'] += amount
         session['amount'] = amount
-        text.insert(0, f"earned {amount} golds from the cave ! {timestamp}")
+        text = (0, f"earned {amount} golds from the cave ! {timestamp}")
     elif method == 'house':
         amount = random.randint(2, 5)
         session['gold'] += amount
         session['amount'] = amount
-        text.insert(0, f"earned {amount} golds from the house ! {timestamp}")
+        text = (0, f"earned {amount} golds from the house ! {timestamp}")
     elif method == 'casino':
         amount = random.randint(-50, 50)
         session['gold'] += amount
         if amount >= 0:
             session['amount'] = amount
-            text.insert(
+            text = (
                 0, f"earned {amount} golds from the casino ! {timestamp}")
         else:
             session['amount'] = amount
-            text.insert(
+            text = (
                 0, f"entered a casino and lost {amount} golds...ouch ! {timestamp}")
-    return redirect('/')
+    session['text'].insert(0,text)
+    return redirect('/',)
 
 
 if __name__ == '__main__':
