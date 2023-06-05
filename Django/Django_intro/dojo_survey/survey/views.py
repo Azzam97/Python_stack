a8b8b23472ca34f9874cb1d1249736c3d1d5dfe0
Azzam Ahmad <azzam.ahmad97@gmail.com>
2023-06-05 22:46:00 +0300

    dojo survey django commit

---
+++ Django/Django_intro/dojo_survey/survey/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+
+# Create your views here.
+
+def form(request):
+    return render(request, 'index.html')
+
+def result(request):
+    print('got post info....')
+    name = request.POST['name']
+    location = request.POST['location']
+    language = request.POST['language']
+    comment = request.POST['comment']
+    gender = request.POST['gender']
+    hobby = request.POST.getlist('hobby')
+    context = {
+        'name': name,
+        'location': location,
+        'language': language,
+        'comment': comment,
+        'gender': gender,
+        'hobby': hobby,
+    }
+    return render(request, 'result.html', context)
+
+def go_back(request):
+    return redirect('/')
+
