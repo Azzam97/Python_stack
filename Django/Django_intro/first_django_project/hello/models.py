a896653016d7d9cf645c104d227fb060e4772d9b
Azzam Ahmad <azzam.ahmad97@gmail.com>
2023-06-08 21:11:55 +0300

    practice with the shell

--- Django/Django_intro/first_django_project/hello/models.py
+++ Django/Django_intro/first_django_project/hello/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class movie(models.Model):
+    title = models.CharField(max_length=45)
+    description = models.TextField()
+    director = models.CharField(max_length=50)
+    release_date = models.DateTimeField()
+    duration = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f"<movie object: {self.title} ({self.id})>"
