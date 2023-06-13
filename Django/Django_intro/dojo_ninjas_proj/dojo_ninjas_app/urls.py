99f0cf9117e389a681e7576d135b03973b7ad8f2
Azzam Ahmad <azzam.ahmad97@gmail.com>
2023-06-13 16:43:46 +0300

    dojo and ninja assignment commit

--- Django/Django_intro/dojo_ninjas_proj/dojo_ninjas_app/urls.py
+++ Django/Django_intro/dojo_ninjas_proj/dojo_ninjas_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('create_dojo', views.create_dojo),
+    path('create_ninja', views.create_ninja)
 ]
