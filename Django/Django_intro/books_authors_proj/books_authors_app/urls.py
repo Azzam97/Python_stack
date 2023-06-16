7b4f51476431cc6455200fe50e399f459e95cdc4
Azzam Ahmad <azzam.ahmad97@gmail.com>
2023-06-16 15:41:13 +0300

    books and authors asignment commit

--- Django/Django_intro/books_authors_proj/books_authors_app/urls.py
+++ Django/Django_intro/books_authors_proj/books_authors_app/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('create_book', views.create_book),
+    path('books/<int:id>', views.books_desc),
+    path('add_author/<int:id>', views.add_author),
+    path('authors', views.authors),
+    path('create_author', views.create_author),
+    path('author/<int:id>', views.authors_desc),
+    path('add_book/<int:id>', views.add_book)
 ]
--- Django/Django_intro/books_authors_proj/books_authors_app/views.py
+++ Django/Django_intro/books_authors_proj/books_authors_app/views.py
@@ -1,6 +1,62 @@
 from django.shortcuts import render, redirect
+from .models import Book, Author
 
 # Create your views here.
 
 def index(request):
-    return "hi"
+    context = {
+        'books': Book.objects.all()
+    }
+    return render(request, "index.html", context)
+
+
+def create_book(request):
+    Book.objects.create(title = request.POST['title'],
+                        desc = request.POST['desc']
+                    )
+    return redirect('/')
+
+
+def books_desc(request,id):
+    context = {
+        'books': Book.objects.get(id = id),
+        'authors': Author.objects.all()
+    }
+    return render(request, "books.html", context)
+
+
+def add_author(request, id):
+    book = Book.objects.get(id = id)
+    author = Author.objects.get(id = int(request.POST['authors']))
+    book.authors.add(author)
+    return redirect(f"/books/{book.id}")
+
+
+def authors(request):
+    context = {
+        'authors': Author.objects.all()
+    }
+    return render(request, "authors.html", context)
+
+
+def create_author(request):
+    Author.objects.create(first_name = request.POST['first_name'],
+                        last_name = request.POST['last_name'],
+                        notes = request.POST['notes']
+                    )
+    return redirect('/authors')
+
+
+def authors_desc(request, id):
+    context = {
+        'authors': Author.objects.get(id = id),
+        'books': Book.objects.all()
+    }
+    return render(request, "author_desc.html", context)
+
+
+def add_book(request, id):
+    author = Author.objects.get(id = id)
+    book = Book.objects.get(id = int(request.POST['books']))
+    author.book.add(book)
+    return redirect(f"/author/{author.id}")
